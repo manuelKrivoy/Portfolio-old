8902920729d290fa21b9d03dc0d347af928a245c
Manuel Krivoy <krivoymanuel@gmail.com>
2024-04-24 15:14:57 -0300

    br mobile

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,12 +7,13 @@ const About = () => {
         <div className="mt-4 md:mt:0 text-left flex flex-col ">
           <div className="my-auto mx-6">
             <h2 className="text-4xl font-bold mb-4 primary-color">Sobre mí</h2>
-            <p className="text-base lg:text-lg">
+            <p className="text-base lg:text-lg ">
               Ser entusiasta por la tecnología desde pequeño me llevó a siempre estar aprendiendo herramientas nuevas.
               Empecé a programar en el secundario, del cual egresé como Técnico En Informática. Actualmente, con 22
               años, estoy cursando tercer año en una Licenciatura en IT y en mis tiempos libres aprendiendo, ya sea de
               forma autodidacta o a través de cursos, las últimas innovaciones tecnológicas.
             </p>
+            <br></br>
           </div>
         </div>
         <Carrousel />
